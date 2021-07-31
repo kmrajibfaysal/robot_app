3e6fada34c345629b6c97dca957164dab9f13f4f
kmrajibfaysal <kmrajibfaysal@gmail.com>
2021-07-31 06:01:59 +0600

    adding searchfield

--- src/App.js
+++ src/App.js
@@ -11,20 +11,22 @@ class App extends Component {
       searchfield: "",
     };
   }
-  onSearchChange(event) {
-    const filteredRobots = this.state.robots.filter((robot) => {
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
+
+  render() {
+    const filteredRobots = this.state.robots.filter((robots) => {
       return robots.name
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
-    console.log(filteredRobots);
-  }
-  render() {
     return (
       <div className="tc">
         <h1>RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={this.state.robots} />
+        <CardList robots={filteredRobots} />
       </div>
     );
   }
