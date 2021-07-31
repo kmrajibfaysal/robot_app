59603ed8e01d1acfa7d0d45fbbb46f672442fa90
kmrajibfaysal <kmrajibfaysal@gmail.com>
2021-07-31 22:45:16 +0600

    adding font and style to h1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import { robots } from "./robots";
+import './App.css';
 
 class App extends Component {
   constructor() {
